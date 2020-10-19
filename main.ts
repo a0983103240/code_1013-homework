f2a1cd8cb17cc7de2e495c4de198e638e55f9e43
a0983103240 <71689449+a0983103240@users.noreply.github.com>
2020-10-19 16:44:06 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,18 +4,12 @@ basic.forever(function () {
     while (Y >= 0) {
         led.plot(X, Y)
         basic.pause(500)
-        if (Y % 2 == 0) {
-            if (X > 0) {
-                X += -1
-            } else {
-                Y += -1
-            }
+        if (Y % 2 == 0 && X > 0) {
+            X += -1
+        } else if (Y % 2 != 0 && X < 4) {
+            X += 1
         } else {
-            if (X < 4) {
-                X += 1
-            } else {
-                Y += -1
-            }
+            Y += -1
         }
     }
 })
